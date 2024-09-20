26176f986251a9b74d4512dd1676eb7d60aeb5eb
Alex McKinstry <alex.mckinstry@fwcs.k12.in.us>
2024-09-20 09:22:45 -0400

    Add files via upload

--- deck_of_cards.py
+++ deck_of_cards.py
@@ -1,11 +1,22 @@
-card_value = [2,3,4,5,6,7,8,9,10,'J','Q','K','A']
-card_suit = ['H', "S", "D","C"]
+import random
 
-deck = []
+def create_deck():
+      deck = []
+      card_value = [2,3,4,5,6,7,8,9,10,'J','Q','K','A']
+      card_suit = ['H', "S", "D","C"]
+      for suit in card_suit:
+            for value in card_value:
+                  x = f"{value}{suit}"
+                  deck.append(x)
+      random.shuffle(deck)
+      #print(deck)
+      
+      return deck
 
-for suit in card_suit:
-      for value in card_value:
-            x = f"{value}{suit}"
-            deck.append(x)
 
-print(deck)
+      
+
+
+new_deck = create_deck()
+
+print(new_deck)

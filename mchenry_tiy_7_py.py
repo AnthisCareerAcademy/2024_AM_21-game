dddc5d3a43cf692409d9a589584a417b4b10a388
brodymchenry <brody.mchenry@fwcsstudents.org>
2024-09-27 08:07:48 -0400

    Add files via upload

--- mchenry_tiy_7_py.py
+++ mchenry_tiy_7_py.py
@@ -1,9 +1,18 @@
-#insert player_score
-#insert dealer_score
-if player_score == 21:
-    if dealer_score == 21:
-        print(tie)
-    else:
-        print(win)
-else:
-    print(...)
+import csv
+filename = 'Chapter_7Challenge.csv'
+with open(filename) as f:
+    reader = csv.reader(f)
+    header_row = next(reader)
+    name = []
+    candy_type_1 = []
+    candy_type_2 = []
+    candy_price_1 = []
+    candy_price_2 = []
+
+    for row in reader:
+        name = row[1]
+        candy_1 = row[2]
+        candy_2 = row[4]
+        price_1 = float(row[3])
+        price_2 = float(row[5])
+
